aa58c4f82e771ebae74647091865f16eaab90b12
e.tokovenko <e.tokovenko@gmail.com>
2015-11-27 21:39:59 +0200

    add modules: about, news and routing

--- frontend/app.js
+++ frontend/app.js
@@ -1,7 +1,18 @@
+import angular from 'angular';
+import router from 'angular-ui-router';
+
 import home from './module/home/home.js';
+import about from './module/about/about.js';
+import news from './module/news/news.js';
 
 import "./css/main.css";
 import "./index.html";
-import angular from 'angular';
 
-angular.module('app', ['app.home']);
+angular
+    .module('app', ['app.about','app.home','app.news','ui.router'])
+    .config(function ($locationProvider) {
+        $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: false
+        });
+    });
