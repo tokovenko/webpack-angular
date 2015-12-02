b7c8eebc971f92097189e039fefb72bfb42838a4
e.tokovenko <e.tokovenko@gmail.com>
2015-12-02 22:29:13 +0200

    add likes for news and news in template

--- frontend/module/news/controller/NewsViewController.js
+++ frontend/module/news/controller/NewsViewController.js
@@ -1,4 +1,9 @@
 let NewsViewController = function($scope, Breadcrumbs, newsItem) {
+
+    newsItem.setLike = function(like) {
+        this.isLiked = like;
+        this.isLiked ? this.likesCount++ : this.likesCount--;
+    };
     $scope.news = newsItem;
 };
 
