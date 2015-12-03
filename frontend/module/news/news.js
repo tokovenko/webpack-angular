99ea40c2fffd6f0efb00113edba22be4d40ef498
e.tokovenko <e.tokovenko@gmail.com>
2015-12-03 19:13:09 +0200

    Divide main.css for modules css

--- frontend/module/news/news.js
+++ frontend/module/news/news.js
@@ -5,6 +5,8 @@ import NewsController from './controller/NewsController';
 import NewsViewController from './controller/NewsViewController';
 import NewsFactory from './factory/News';
 
+import css from './css/news.css';
+
 export default angular
     .module('app.news', ['ui.router', 'app.news.factory'])
     .config(routes)
